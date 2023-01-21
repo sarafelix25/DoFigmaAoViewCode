b27433d05f4f141e190f112b010352bbdb1d2e6d
Sara Felix <sfelix@lifescan.com>
2023-01-21 11:30:56 -0300

    init

---
+++ FigmaAoViewCode/FigmaAoViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FigmaAoViewCode
+//
+//  Created by Sara Batista dos Santos Felix on 1/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
